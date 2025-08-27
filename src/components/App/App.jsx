c42dd92415ba7a988e11c8352be021cd4bfd8a73
Marc Pickelman <marcpc@att.net>
2025-08-27 16:31:48 -0500

    Add new feature: ModalWithForm

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -18,7 +18,52 @@ function App() {
         <Main weatherData={weatherData} />
         <Footer />
       </div>
-      <ModalWithForm />
+      <ModalWithForm title="New garment" buttonText="Add garment">
+        <label htmlFor="name" className="modal__label">
+            Name
+            <input
+              id="name"
+              type="text"
+              className="modal__input"
+              placeholder="Name"
+            ></input>
+          </label>
+          <label htmlFor="imageURL" className="modal__label">
+            Image
+            <input
+              id="imageURL"
+              type="url"
+              className="modal__input"
+              placeholder="Image URL"
+            ></input>
+          </label>
+          <fieldset className="modal__radio-buttons">
+            <legend className="modal__legend">Select the weather type:</legend>
+            <label
+              htmlFor="hot"
+              className="modal__label modal__label_type_radio"
+            >
+              <input id="hot" type="radio" className="modal__radio-input" /> Hot
+              <span className="modal__radio-span_type_black"></span>
+            </label>
+            <label
+              htmlFor="warm"
+              className="modal__label modal__label_type_radio"
+            >
+              <input id="warm" type="radio" className="modal__radio-input" />{" "}
+              Warm
+              <span className="modal__radio-span_type_black"></span>
+            </label>
+            <label
+              htmlFor="cold"
+              className="modal__label modal__label_type_radio"
+            >
+              <input id="cold" type="radio" className="modal__radio-input" />{" "}
+              Cold
+              <span className="modal__radio-span_type_black"></span>
+            </label>
+          </fieldset>
+      </ModalWithForm>
     </div>
   );
 }
--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,52 +1,19 @@
 import "./ModalWithForm.css";
 import closeIcon from "../../assets/close-icon.png";
 
-function ModalWithForm() {
+function ModalWithForm({ children, title, buttonText }) {
   return (
     <div className="modal">
-      <form className="modal__form">
-        <h2 className="modal__title">New garment</h2>
-        <button className="modal__close" type="button" src={closeIcon}></button>
-        <label htmlFor="name" className="modal__label">
-          Name
-          <input
-            id="name"
-            type="text"
-            className="modal__input"
-            placeholder="Name"
-          ></input>
-        </label>
-        <label htmlFor="imageURL" className="modal__label">
-          Image
-          <input
-            id="imageURL"
-            type="url"
-            className="modal__input"
-            placeholder="Image URL"
-          ></input>
-        </label>
-        <fieldset className="modal__radio-buttons">
-          <legend className="modal__legend">Select the weather type:</legend>
-          <label htmlFor="hot" className="modal__label modal__label_type_radio">
-            <input id="hot" type="radio" className="modal__radio-input" /> Hot
-          </label>
-          <label
-            htmlFor="warm"
-            className="modal__label modal__label_type_radio"
-          >
-            <input id="warm" type="radio" className="modal__radio-input" /> Warm
-          </label>
-          <label
-            htmlFor="cold"
-            className="modal__label modal__label_type_radio"
-          >
-            <input id="cold" type="radio" className="modal__radio-input" /> Cold
-          </label>
-        </fieldset>
-        <button className="modal__submit-button" type="submit">
-          Add garment
-        </button>
-      </form>
+      <div className="modal__content">
+        <h2 className="modal__title">{title}</h2>
+        <button className="modal__close" type="button"></button>
+        <form className="modal__form">
+          {children}
+          <button className="modal__submit-button" type="submit">
+            {buttonText}
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
