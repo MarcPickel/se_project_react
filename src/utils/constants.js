3b6152d2c110a0f9195cc1a165afc35b4a757a26
Marc Pickelman <marcpc@att.net>
2025-08-28 03:40:40 -0500

    Add images for Weather Cards

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,66 @@
+export const weatherOptions = [
+  {
+    day: true,
+    condition: "clear",
+    url: new URL("../assets", import.meta.url),
+  },
+  {
+    day: true,
+    condition: "cloudy",
+    url: new URL("", import.meta.url),
+  },
+  {
+    day: true,
+    condition: "foggy",
+    url: new URL("", import.meta.url),
+  },
+  {
+    day: true,
+    condition: "rainy",
+    url: new URL("", import.meta.url),
+  },
+  {
+    day: true,
+    condition: "snowy",
+    url: new URL("", import.meta.url),
+  },
+  {
+    day: true,
+    condition: "stormy",
+    url: new URL("", import.meta.url),
+  },
+  {
+    day: false,
+    condition: "clear",
+    url: new URL("", import.meta.url),
+  },
+  {
+    day: false,
+    condition: "cloudy",
+    url: new URL("", import.meta.url),
+  },
+  {
+    day: false,
+    condition: "foggy",
+    url: new URL("", import.meta.url),
+  },
+  {
+    day: false,
+    condition: "rainy",
+    url: new URL("", import.meta.url),
+  },
+  {
+    day: false,
+    condition: "snowy",
+    url: new URL("", import.meta.url),
+  },
+  {
+    day: false,
+    condition: "stormy",
+    url: new URL("", import.meta.url),
+  },
+];
+
 const coordinates = {
   latitude: 43.4889637,
   longitude: -96.721992,
