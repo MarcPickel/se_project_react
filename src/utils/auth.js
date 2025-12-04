7d27806fdc6716745fddab7b99c122c2d7bae189
MarcPickel <marcpc@att.net>
2025-12-04 05:33:14 -0600

    Update: homepage to match backend

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,7 @@
-const baseUrl = "http://localhost:3001";
+const baseUrl = process.env.NODE_ENV === "production" 
+  ? "https://api.what.to.wear.jumpingcrab.com"
+  : "http://localhost:3001";
+
 import { checkResponse } from "./api";
 
 const register = (email, password, name, avatar) => {
